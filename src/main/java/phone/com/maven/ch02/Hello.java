7ee5220b6a53b17cc1518c1c7c13ef2d4bd273d9
JWRE <1356055459@qq.com>
2018-04-04 12:11:40 +0800

    aa

--- src/main/java/phone/com/maven/ch02/Hello.java
+++
@@ -1,9 +0,0 @@
-package phone.com.maven.ch02;
-import phone.com.maven.HelloMaven;
-public class Hello
-{
-	public String say(String name){
-		HelloMaven hm = new HelloMaven();
-		return hm.sayHello(name);
-	}
-}
